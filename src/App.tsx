8f1e0b09529eff03fbf0e624596940e9b813a670
Angelo Colangelo <acolange@gmail.com>
2017-12-19 01:06:02 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+const logo = require('./logo.svg');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2></h2>
+      </div>
+    );
+  }
+}
+
+export default App;
